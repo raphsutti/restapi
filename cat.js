f79270e4845ad5aabe1da91ccf01c3e6d6f65ae0
Raph Sutti <neoster9@gmail.com>
2018-07-03 07:11:59 +0000

    cat database

--- cat.js
+++ cat.js
@@ -13,20 +13,41 @@ var catSchema = new mongoose.Schema({
 var Cat = mongoose.model("Cat", catSchema);
 
 // add new cat to DB
-var george = new Cat({
-    name: "George",
-    age: 11,
-    temperament: "Grouchy"
-});
+// var george = new Cat({
+//     name: "Mrs. Norris",
+//     age: 7,
+//     temperament: "Evil"
+// });
 
-george.save(function(err, cat){
-    if(err){
-        console.log("SOMETHING WENT WRONG!")
+// george.save(function(err, cat){
+//     if(err){
+//         console.log("SOMETHING WENT WRONG!")
+//     } else {
+//         console.log("WE JUST SAVED A CAT TO THE DB:")
+//         console.log(cat);
+//     }
+// });
+
+// use create for new and save
+Cat.create({
+    name: "Snow White",
+    age: 15,
+    temperament: "Bland"
+}, function(err, cat){
+    if(err) {
+        console.log(err);
     } else {
-        console.log("WE JUST SAVED A CAT TO THE DB:")
         console.log(cat);
     }
 });
 
-
 // retrieve all cats from DB and console log each one
+Cat.find({}, function(err, cats){
+    if(err){
+        console.log("OH NO, ERROR!");
+        console.log(err);
+    } else {
+        console.log("ALL THE CATS...");
+        console.log(cats);
+    }
+})
